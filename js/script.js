925c4013eb8f290c6f4a5b78fa348e5bb9c779df
thiagogomes-tg <thiagofioravantegomes86@gmail.com>
2025-05-19 11:24:08 -0300

    feat: ajustes finais e adicionando comentários no código todo (html, css e js)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
+// Seleciona o elemento 'form'
 document.addEventListener("DOMContentLoaded", function () {
     const formulario = document.querySelector('form');
 
+    // Adiciona um evento de escuta para o envio do formulário
     formulario.addEventListener('submit', function (event) {
-        event.preventDefault(); // Impede o envio padrão
+        event.preventDefault(); // Impede o envio padrão para evitar recarregar a página
         alert('Mensagem enviada! Obrigado por entrar em contato.');
         formulario.reset();
     });
